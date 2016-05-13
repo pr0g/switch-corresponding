05c22698d2c9f6f6f51cbb9bfb01b9e033e1e3c1
zabel-xyz <zabel.xyz@gmail.com>
2016-05-13 20:23:14 +0200

    FindFiles (2 separated commands)

--- src/extension.ts
+++ src/extension.ts
@@ -15,15 +15,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.switch_corresponding', () => {
+
+	var disposable = vscode.commands.registerCommand('extension.switch_corresponding', () => switch_corresponding() );
+	context.subscriptions.push(disposable);
+	var disposable = vscode.commands.registerCommand('extension.switch_corresponding_sameDirectory', () => switch_corresponding(true) );
+	context.subscriptions.push(disposable);
+
+	function switch_corresponding(sameDirectory?: boolean) {
+
 		var filePath = vscode.window.activeTextEditor.document.fileName;
 		var fileNameAndExtension = path.basename(filePath);
 		var	fileName = path.basename(filePath, path.extname(filePath));
 
-		if (<boolean>vscode.workspace.getConfiguration('switch-corresponding').get('sameFolder')) {
-			// Search in same folder
-			// Add '.' in fileName because unit.pas must not switch to unit_3.pas
-			fileName = fileName +'.*';
+		if (sameDirectory) {
 			// build relative path
 			let dir = path.dirname(filePath),
 				relative = vscode.workspace.asRelativePath(dir);
@@ -31,14 +35,13 @@ export function activate(context: vscode.ExtensionContext) {
 				fileName = path.join(relative, fileName).substr(1).replace(/\\/g, '/');
 				fileNameAndExtension = path.join(relative, fileNameAndExtension).substr(1).replace(/\\/g, '/');
 			}
+			fileName = fileName + ".*";
 		} else {
-			// Search in all folders
-			fileName = "**/" + fileName + "*";
+			fileName = "**/" + fileName + ".*";
 			fileNameAndExtension =  "**/" + fileNameAndExtension;
 		}
 
 		var files = vscode.workspace.findFiles(fileName, fileNameAndExtension, 100);
-
 		files.then((value) => {
 			for (var i = 0; i < value.length; ++i) {
 				var textDocument = vscode.workspace.openTextDocument(value[i]);
@@ -50,10 +53,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}, (reason) => {
 			console.log(reason);
 		});
-
-	});
-
-	context.subscriptions.push(disposable);
+	}
 }
 
 // this method is called when your extension is deactivated
